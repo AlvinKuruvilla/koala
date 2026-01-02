f630e6dfc8bd7bbe387a20847a0ee4b2536dd1fe
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2026-01-01 21:58:57 -0500

    handle_in_head_noscript_mode

--- crates/koala-html/src/parser/parser.rs
+++ crates/koala-html/src/parser/parser.rs
@@ -442,7 +442,7 @@ impl HTMLParser {
             InsertionMode::InHead => self.handle_in_head_mode(token),
             InsertionMode::InHeadNoscript => {
                 // TODO: [§ 13.2.6.4.5](https://html.spec.whatwg.org/multipage/parsing.html#parsing-main-inheadnoscript)
-                todo!("InHeadNoscript mode")
+                self.handle_in_head_noscript_mode(token)
             }
             InsertionMode::AfterHead => self.handle_after_head_mode(token),
             InsertionMode::InBody => self.handle_in_body_mode(token),
@@ -1282,7 +1282,85 @@ impl HTMLParser {
         // STEP 3: "Reprocess the token."
         self.reprocess_token(token);
     }
+    fn handle_in_head_noscript_mode(&mut self, token: &Token) {
+        // A DOCTYPE token
+        match token {
+            Token::Doctype { .. } => {
+                // TODO: Parse error. Ignore the token.
+            }
+            // A start tag whose tag name is "html"
+            Token::StartTag { name, .. } if name == "html" => {
+                // Process the token using the rules for the "in body" insertion mode.
+                self.handle_in_body_mode(token);
+            }
+            // "An end tag whose tag name is "noscript""
+            // "Pop the current node (which will be a noscript element) from the stack of
+            //  open elements; the new current node will be a head element."
+            // "Switch the insertion mode to "in head"."
+            Token::EndTag { name, .. } if name == "noscript" => {
+                let _ = self.stack_of_open_elements.pop();
+                self.insertion_mode = InsertionMode::InHead;
+            }
 
+            // "A character token that is one of U+0009 CHARACTER TABULATION, U+000A LINE FEED (LF),
+            //  U+000C FORM FEED (FF), U+000D CARRIAGE RETURN (CR), or U+0020 SPACE"
+            // "Process the token using the rules for the "in head" insertion mode."
+            Token::Character { data } if Self::is_whitespace(*data) => {
+                self.handle_in_head_mode(token);
+            }
+
+            // "A comment token"
+            // "Process the token using the rules for the "in head" insertion mode."
+            Token::Comment { .. } => {
+                self.handle_in_head_mode(token);
+            }
+
+            // "A start tag whose tag name is one of: "basefont", "bgsound", "link", "meta",
+            //  "noframes", "style""
+            // "Process the token using the rules for the "in head" insertion mode."
+            Token::StartTag { name, .. }
+                if matches!(
+                    name.as_str(),
+                    "basefont" | "bgsound" | "link" | "meta" | "noframes" | "style"
+                ) =>
+            {
+                self.handle_in_head_mode(token);
+            }
+
+            // "An end tag whose tag name is "br""
+            // "Act as described in the "anything else" entry below."
+            Token::EndTag { name, .. } if name == "br" => {
+                // Pop the current node (which will be a noscript element) from the stack of open elements.
+                let _ = self.stack_of_open_elements.pop();
+                // Switch the insertion mode to "in head".
+                self.insertion_mode = InsertionMode::InHead;
+                // Reprocess the token.
+                self.reprocess_token(token);
+            }
+
+            // "A start tag whose tag name is one of: "head", "noscript""
+            // "Any other end tag"
+            // "Parse error. Ignore the token."
+            Token::StartTag { name, .. } if matches!(name.as_str(), "head" | "noscript") => {
+                // Parse error. Ignore the token.
+            }
+            Token::EndTag { .. } => {
+                // Parse error. Ignore the token.
+            }
+
+              // Anything else
+            _ => {
+              // TODO: Parse error.
+
+              // Pop the current node (which will be a noscript element) from the stack of open elements; the new current node will be a head element.
+                let _ = self.stack_of_open_elements.pop();
+              // Switch the insertion mode to "in head".
+                self.insertion_mode = InsertionMode::InHead;
+              // Reprocess the token.
+                self.reprocess_token(token);
+            }
+        }
+    }
     /// [§ 13.2.6.4.8 The "text" insertion mode](https://html.spec.whatwg.org/multipage/parsing.html#parsing-main-incdata)
     fn handle_text_mode(&mut self, token: &Token) {
         match token {
